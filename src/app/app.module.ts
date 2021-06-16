b4f653d4614e375bff91a793c39b816206efe1a6
juan-para <juanpara86@gmail.com>
2021-06-16 14:08:13 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.components';
+
+@NgModule({
+  declarations: [AppComponent, ProductListComponent],
+  imports: [BrowserModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
