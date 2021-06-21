aedcaefbce9569d18ab307a831c5facafb096ce1
juan-para <juanpara86@gmail.com>
2021-06-21 19:09:41 -0300

    Added custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.components';
+import { ConvertToSpacesPipes } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipes],
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
